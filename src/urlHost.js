2a31b89b2965588103e6ee3549f576967aba1205
Lady Meek <ladyjmeek@gmail.com>
2023-06-25 16:38:30 -0700

    troubleshooting login again

--- src/urlHost.js
+++ src/urlHost.js
@@ -1,4 +1,4 @@
 export const URL_HOST = 
     process.env.NODE_ENV === "production"
-        ? "https://ladys-proxy.ladyjmeek.workers.dev/?targetUrl=http://capstonebackend-env.eba-djw3wpfj.us-east-2.elasticbeanstalk.com/"
+        ? "https://ladys-proxy.ladyjmeek.workers.dev/?targetUrl=http://capstonebackend-env.eba-djw3wpfj.us-east-2.elasticbeanstalk.com"
         : "http://127.0.0.1:8000";
